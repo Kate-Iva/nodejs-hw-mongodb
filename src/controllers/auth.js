216a5a811cfc0c11ddab4d2b1cdbd846448e1864
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-09-17 15:36:42 +0100

    register and login added

---
+++ src/controllers/auth.js
@@ -0,0 +1,33 @@
+import * as authServices from "../services/auth.js";
+
+export const registerController = async(req, res)=> {
+    const newUser = await authServices.register(req.body);
+
+    res.status(201).json({
+        status: 201,
+        message: "Successfully registered a user!",
+        data: newUser,
+    });
+};
+
+export const loginController = async(req, res)=> {
+    const session = await authServices.login(req.body);
+
+    res.cookie("refreshToken", session.refreshToken, {
+        httpOnly: true,
+        expire: new Date(Date.now() + session.refreshTokenValidUntil),
+    });
+
+    res.cookie("sessionId", session._id, {
+        httpOnly: true,
+        expire: new Date(Date.now() + session.refreshTokenValidUntil),
+    });
+
+    res.json({
+        status: 200,
+        message: "Successfully login",
+        data: {
+            accessToken: session.accessToken,
+        }
+    });
+};
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import logger from "./middlewares/logger.js";
 
+import authRouter from "./routers/auth.js";
 import contactsRouter from "./routers/contacts.js";
 
 export const setupServer = () => {
@@ -17,6 +18,7 @@ export const setupServer = () => {
   app.use(express.json());
 
   // 
+  app.use("/auth", authRouter);
   app.use("/contacts", contactsRouter);
 
   //
